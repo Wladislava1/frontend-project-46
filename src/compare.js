72e40ad79b5ce993f421fdf33b5b543f3423b1d6
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2024-12-29 17:21:57 +0300

    output string

--- src/compare.js
+++ src/compare.js
@@ -4,29 +4,25 @@ const compare = (obj1, obj2) => {
 
     const sortObj1 = sortAlphabet(obj1);
     const sortObj2 = sortAlphabet(obj2);
-    const resultObj = {};
+    const resultObj = [];
     for (const variable in sortObj1){
       if (sortObj2[variable] !== undefined && sortObj1[variable] === sortObj2[variable]){
-        resultObj[variable] = sortObj1[variable]
+        resultObj.push(`    ${variable}: ${sortObj1[variable]}`);
       } 
       if (sortObj2[variable] === undefined) {
-        resultObj[`- ${variable}`] = sortObj1[variable]
+        resultObj.push(`  - ${variable}: ${sortObj1[variable]}`);
       }
       if (sortObj2[variable] !== undefined && sortObj1[variable] !== sortObj2[variable]){
-        resultObj[`- ${variable}`] = sortObj1[variable]
-        resultObj[`+ ${variable}`] = sortObj2[variable]
+        resultObj.push(`  - ${variable}: ${sortObj1[variable]}`);
+        resultObj.push(`  + ${variable}: ${sortObj2[variable]}`);
       }
     }
     for (const variable in sortObj2){
       if (sortObj1[variable] === undefined) {
-        resultObj[`+ ${variable}`] = sortObj2[variable]
-      }
-      if (sortObj1[variable] !== undefined && sortObj1[variable] !== sortObj2[variable]){
-        resultObj[`- ${variable}`] = sortObj1[variable]
-        resultObj[`+ ${variable}`]  = sortObj2[variable]
+        resultObj.push(`  + ${variable}: ${sortObj2[variable]}`);
       }
     }
-    return resultObj;
+    return `{\n${resultObj.join('\n')}\n}`;
  }
 
 export default compare;
