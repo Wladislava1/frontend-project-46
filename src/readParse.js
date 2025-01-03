b619ff9b6afa838151cddb90ff3ea9b41d399a21
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 17:38:08 +0300

    resolve path

--- src/readParse.js
+++ src/readParse.js
@@ -1,10 +1,6 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
-const parseFile = (pathTofile) => {
-  if (path.isAbsolute(pathTofile)) {
-    return JSON.parse(readFileSync(pathTofile, 'utf-8'));
-  }
-  return JSON.parse(readFileSync(path.resolve(pathTofile)));
-};
+const parseFile = (pathTofile) => JSON.parse(readFileSync(path.resolve(pathTofile)));
+
 export default parseFile;
