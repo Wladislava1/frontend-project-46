af567f6a8622c06b719d983cca7a42475cc64586
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 19:43:53 +0300

    __dirname mb

--- src/readParse.js
+++ src/readParse.js
@@ -1,8 +1,12 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const parseFile = (pathToFile) => {
-  const absolutePath = path.resolve(process.cwd(), pathToFile);
+  const absolutePath = path.resolve(__dirname, '..', pathToFile);
   return JSON.parse(readFileSync(absolutePath, 'utf-8'));
 };
 export default parseFile;
