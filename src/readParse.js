e9ecad80ca8f4c7e465f8ff5a5f387c4e0aa1784
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 18:33:20 +0300

    relative

--- src/readParse.js
+++ src/readParse.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import * as path from 'path';
 
 const parseFile = (pathToFile) => {
-  const absolutePath = path.resolve(process.cwd(), pathToFile);
+  const absolutePath = path.relative(process.cwd(), pathToFile);
   return JSON.parse(readFileSync(absolutePath, 'utf-8'));
 };
 export default parseFile;
