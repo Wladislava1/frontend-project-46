9d15e2fdbb2a7c0e08406b35275135301a60fb97
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 17:56:38 +0300

    add process.cwd() in path.relative

--- src/readParse.js
+++ src/readParse.js
@@ -1,6 +1,8 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
-const parseFile = (pathTofile) => JSON.parse(readFileSync(path.resolve(process.cwd(), pathTofile)));
-
+const parseFile = (pathTofile) => {
+  const relativePath = path.relative(process.cwd(), pathTofile);
+  return JSON.parse(readFileSync(relativePath));
+};
 export default parseFile;
