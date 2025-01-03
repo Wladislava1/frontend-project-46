8a6f0626aa4308c914b54d37d34c8d1c2e5f11ab
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 17:45:30 +0300

    add process.cwd()

--- src/readParse.js
+++ src/readParse.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
-const parseFile = (pathTofile) => JSON.parse(readFileSync(path.resolve(pathTofile)));
+const parseFile = (pathTofile) => JSON.parse(readFileSync(path.resolve(process.cwd(), pathTofile)));
 
 export default parseFile;
