0feb1dc82aecd6ee8d384207863ba68898e5b5c6
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-04 00:47:17 +0300

    check relativePath

--- __test__/readParse.test.js
+++ __test__/readParse.test.js
@@ -1,9 +1,8 @@
 import parseFile from '../src/readParse.js';
 import {
-  resolvePath, relativePath, parseRelativePath, parseResolvePath,
+  relativePath, parseRelativePath,
 } from '../__fixtures__/tests.js';
 
 test('read and parse', () => {
-  expect(parseFile(resolvePath)).toEqual(parseResolvePath);
   expect(parseFile(relativePath)).toEqual(parseRelativePath);
 });
