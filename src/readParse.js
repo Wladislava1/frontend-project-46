370ba86e3de88e9592169d6521f425383f32b572
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 18:30:20 +0300

    i don't know

--- src/readParse.js
+++ src/readParse.js
@@ -1,9 +1,8 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
-import { workDirectory } from '../__fixtures__/tests.js';
 
-const parseFile = (pathTofile) => {
-  const relativePathNew = path.resolve(workDirectory, pathTofile);
-  return JSON.parse(readFileSync(relativePathNew));
+const parseFile = (pathToFile) => {
+  const absolutePath = path.resolve(process.cwd(), pathToFile);
+  return JSON.parse(readFileSync(absolutePath, 'utf-8'));
 };
 export default parseFile;
