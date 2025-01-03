39c9593a9c9cde98e344cb092d7fb52cb621e5e4
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 18:37:53 +0300

    add workingirrectory

--- src/readParse.js
+++ src/readParse.js
@@ -1,8 +1,9 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
+const workDirectory = process.env.GITHUB_WORKSPACE || process.cwd();
 const parseFile = (pathToFile) => {
-  const absolutePath = path.relative(process.cwd(), pathToFile);
+  const absolutePath = path.relative(workDirectory, pathToFile);
   return JSON.parse(readFileSync(absolutePath, 'utf-8'));
 };
 export default parseFile;
