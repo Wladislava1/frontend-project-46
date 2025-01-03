d3f910f2b7457f19b5f71c4a44c9f10f2b8d1dfd
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-03 18:57:17 +0300

    __dirname

--- src/readParse.js
+++ src/readParse.js
@@ -1,9 +1,13 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const workDirectory = process.env.GITHUB_WORKSPACE || process.cwd();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const parseFile = (pathToFile) => {
-  const absolutePath = path.relative(workDirectory, pathToFile);
+  const absolutePath = path.resolve(__dirname, '..', pathToFile);
   return JSON.parse(readFileSync(absolutePath, 'utf-8'));
 };
 export default parseFile;
