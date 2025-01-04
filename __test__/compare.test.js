0fca21d33a87039686137f54c8b36903945e2092
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-04 15:23:08 +0300

    add description for tests

--- __test__/compare.test.js
+++ __test__/compare.test.js
@@ -3,7 +3,7 @@ import {
   json1, json2, result, result1,
 } from '../__fixtures__/tests.js';
 
-test('compare two files json', () => {
+test('compare two files json and yaml', () => {
   expect(compare(json1, json2)).toEqual(result);
   expect(compare({}, {})).toEqual(result1);
 });
--- __test__/sortAlphabet.test.js
+++ __test__/sortAlphabet.test.js
@@ -1,10 +1,10 @@
 import sortAlphabet from '../src/sortAlphabet.js';
 import {
-  json1Sort, json2Sort, json1, json2, result2,
+  json1Sort, json2Sort, json1, json2,
 } from '../__fixtures__/tests.js';
 
-test('alphabetically sorted', () => {
+test('alphabetically sorted json and yaml', () => {
   expect(sortAlphabet(json1)).toEqual(json1Sort);
   expect(sortAlphabet(json2)).toEqual(json2Sort);
-  expect(sortAlphabet({})).toEqual(result2);
+  expect(sortAlphabet({})).toEqual({});
 });
