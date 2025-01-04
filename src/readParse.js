61d4f85b2a107ff987364245ec52acc766a53355
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-04 15:22:21 +0300

    add check extname filepath => .yaml || .json

--- src/readParse.js
+++ src/readParse.js
@@ -1,5 +1,11 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import yaml from 'js-yaml';
 
-const parseFile = (pathToFile) => JSON.parse(readFileSync(path.resolve(process.cwd(), pathToFile)));
+const parseFile = (pathToFile) => {
+  if (path.extname(pathToFile) === '.yaml' || path.extname(pathToFile) === '.yml') {
+    return yaml.load(readFileSync(path.resolve(process.cwd(), pathToFile)));
+  }
+  return JSON.parse(readFileSync(path.resolve(process.cwd(), pathToFile)));
+};
 export default parseFile;
