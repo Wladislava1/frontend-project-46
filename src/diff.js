2f86a9bd8558a2a08de29d6d8705deb7caed5daa
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-11 19:49:27 +0300

    code for finding file differences

--- src/diff.js
+++ src/diff.js
@@ -1,20 +1,25 @@
+import _ from 'lodash';
 import sortObj from './sortDiff.js';
 
 const findDifferences = (obj1, obj2) => {
   const differences = {};
-
   Object.keys(obj1).forEach((key) => {
     if (obj2[key] === undefined) {
       differences[key] = { value: obj1[key], type: 'removed' };
     } else if (obj1[key] !== obj2[key]) {
-      if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-        const nestedDiffs = findDifferences(obj1[key], obj2[key]);
-        if (Object.keys(nestedDiffs).length > 0) {
-          differences[key] = nestedDiffs;
-        }
+      if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+        differences[key] = {
+          value1: obj1[key],
+          value2: obj2[key],
+          type: 'changed',
+        };
+      } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        differences[key] = findDifferences(obj1[key], obj2[key]);
       } else {
         differences[key] = { value1: obj1[key], value2: obj2[key], type: 'changed' };
       }
+    } else {
+      differences[key] = { value: obj1[key], type: 'unchanged' };
     }
   });
 
