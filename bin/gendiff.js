ad2a8722bcff08a74a45c364e0f5ce216c525496
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-11 19:53:36 +0300

    add default options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 import { Command } from 'commander';
 import parseFile from '../src/readParse.js';
 import compare from '../src/compare.js';
+import formarterNotFlat from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,11 +10,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', formarterNotFlat)
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(compare(parseFile(filepath1), parseFile(filepath2)));
+  .action((filepath1, filepath2, options) => {
+    const format = options.format || formarterNotFlat;
+    console.log(compare(parseFile(filepath1), parseFile(filepath2), format));
     if (!filepath1 || !filepath2) {
       console.log(`Usage: gendiff [options] ${filepath1} ${filepath2}`);
     }
