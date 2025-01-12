be1a3f617fabb04342932bc7844e6d8b27c8e894
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-13 01:49:58 +0300

    ubdate logic

---
+++ src/formatters/index.js
@@ -0,0 +1,22 @@
+import findDifferences from '../diff.js';
+import sortObj from '../sortDiff.js';
+import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
+
+const compare = (obj1, obj2, format = stylish) => {
+  const sortObj1 = sortObj(obj1);
+  const sortObj2 = sortObj(obj2);
+  const diff = findDifferences(sortObj1, sortObj2);
+
+  if (format === stylish) {
+    return `{\n${stylish(diff)}\n}`;
+  } if (format === plain) {
+    return `\n${plain(diff).join('\n')}\n`;
+  } if (format === json) {
+    return json(diff);
+  }
+  return `Unknown format: ${format}`;
+};
+
+export default compare;
