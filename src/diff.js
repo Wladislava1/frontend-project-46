9bdb0a2e61c1dd54f232c5cdfd00c541cfc30be6
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-13 01:47:49 +0300

    update diff => add full path with every value

--- src/diff.js
+++ src/diff.js
@@ -1,33 +1,39 @@
 import _ from 'lodash';
 import sortObj from './sortDiff.js';
 
-const findDifferences = (obj1, obj2) => {
+const findDifferences = (obj1, obj2, path = '') => {
   const differences = {};
+
   Object.keys(obj1).forEach((key) => {
+    const currentPath = path ? `${path}.${key}` : key;
+
     if (obj2[key] === undefined) {
-      differences[key] = { value: obj1[key], type: 'removed' };
+      differences[currentPath] = { value: obj1[key], type: 'removed' };
     } else if (obj1[key] !== obj2[key]) {
       if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-        differences[key] = {
+        differences[currentPath] = {
           value1: obj1[key],
           value2: obj2[key],
           type: 'changed',
         };
       } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        differences[key] = findDifferences(obj1[key], obj2[key]);
+        Object.assign(differences, findDifferences(obj1[key], obj2[key], currentPath));
       } else {
-        differences[key] = { value1: obj1[key], value2: obj2[key], type: 'changed' };
+        differences[currentPath] = { value1: obj1[key], value2: obj2[key], type: 'changed' };
       }
     } else {
-      differences[key] = { value: obj1[key], type: 'unchanged' };
+      differences[currentPath] = { value: obj1[key], type: 'unchanged' };
     }
   });
 
   Object.keys(obj2).forEach((key) => {
+    const currentPath = path ? `${path}.${key}` : key;
     if (obj1[key] === undefined) {
-      differences[key] = { value: obj2[key], type: 'added' };
+      differences[currentPath] = { value: obj2[key], type: 'added' };
     }
   });
+
   return sortObj(differences);
 };
+
 export default findDifferences;
