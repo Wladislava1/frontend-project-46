879e642a37858c1dc6da44a08c6e64b74dbc92c1
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-13 02:28:13 +0300

    C => c in [Complex value]

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -15,17 +15,17 @@ const plain = (objDiff) => {
       differences.push(`Property '${key}' was removed`);
     } else if (value.type === 'added') {
       if (_.isPlainObject(value.value)) {
-        differences.push(`Property '${key}' was added with value: [Complex value]`);
+        differences.push(`Property '${key}' was added with value: [complex value]`);
       } else {
         differences.push(`Property '${key}' was added with value: ${formatValue(value.value)}`);
       }
     } else if (value.type === 'changed') {
       if (_.isPlainObject(value.value1) && _.isPlainObject(value.value2)) {
-        differences.push(`Property '${key}' was updated. From [Complex value] to [Complex value]`);
+        differences.push(`Property '${key}' was updated. From [complex value] to [complex value]`);
       } else if (!_.isPlainObject(value.value1) && _.isPlainObject(value.value2)) {
-        differences.push(`Property '${key}' was updated. From ${formatValue(value.value1)} to [Complex value]`);
+        differences.push(`Property '${key}' was updated. From ${formatValue(value.value1)} to [complex value]`);
       } else if (_.isPlainObject(value.value1) && !_.isPlainObject(value.value2)) {
-        differences.push(`Property '${key}' was updated. From [Complex value] to ${formatValue(value.value2)}`);
+        differences.push(`Property '${key}' was updated. From [complex value] to ${formatValue(value.value2)}`);
       } else {
         differences.push(`Property '${key}' was updated. From ${formatValue(value.value1)} to ${formatValue(value.value2)}`);
       }
