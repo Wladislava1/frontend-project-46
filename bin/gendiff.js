92b62352b1a9ae2f59f3ce2de083f4fa198b26b5
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-13 01:46:52 +0300

    2 formatters add

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import parseFile from '../src/readParse.js';
-import compare from '../src/compare.js';
-import formarterNotFlat from '../src/stylish.js';
+import compare from '../src/formatters/index.js';
+import plain from '../src/formatters/plain.js';
+import json from '../src/formatters/json.js';
 
 const program = new Command();
 
@@ -10,12 +11,17 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', formarterNotFlat)
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
-    const format = options.format || formarterNotFlat;
-    console.log(compare(parseFile(filepath1), parseFile(filepath2), format));
+    if (options.format === 'plain') {
+      console.log(compare(parseFile(filepath1), parseFile(filepath2), plain));
+    } else if (options.format === 'json') {
+      console.log(compare(parseFile(filepath1), parseFile(filepath2), json));
+    } else {
+      console.log(compare(parseFile(filepath1), parseFile(filepath2)));
+    }
     if (!filepath1 || !filepath2) {
       console.log(`Usage: gendiff [options] ${filepath1} ${filepath2}`);
     }
