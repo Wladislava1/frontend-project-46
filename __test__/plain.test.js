08fc0a80cc9d3a82e1b1b9094021ad1e7d75bc56
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-15 21:42:34 +0300

    delete unnecessary files

--- __test__/plain.test.js
+++
@@ -1,7 +0,0 @@
-import { diffrentJsonsSort, plainResult } from '../__fixtures__/tests.js';
-import plain from '../src/formatters/plain.js';
-
-test('formatter plain', () => {
-  expect(plain(diffrentJsonsSort)).toEqual(plainResult);
-  expect(plain([])).toEqual([]);
-});
--- __test__/sortDiff.test.js
+++
@@ -1,9 +0,0 @@
-import sortObj from '../src/sortDiff.js';
-import {
-  diffrentJsons, diffrentJsonsSort,
-} from '../__fixtures__/tests.js';
-
-test('alphabetically sorted diff', () => {
-  expect(sortObj(diffrentJsons)).toEqual(diffrentJsonsSort);
-  expect(sortObj({})).toEqual({});
-});
