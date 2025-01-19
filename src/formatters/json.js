fcf6a7af27ec7c7aaa73871e2729e27e777d1ad3
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-19 17:18:22 +0300

    fix json muttation

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,32 +1,30 @@
-const json = (diff) => {
-  const result = Object.entries(diff).reduce((acc, [key, change]) => {
-    const newEntry = {};
-
-    switch (change.type) {
-      case 'added':
-        newEntry[key] = { status: 'added', value: change.value };
-        break;
-      case 'removed':
-        newEntry[key] = { status: 'removed', value: change.value };
-        break;
-      case 'changed':
-        newEntry[key] = {
-          status: 'changed',
-          oldValue: change.value1,
-          newValue: change.value2,
-        };
-        break;
-      case 'unchanged':
-        newEntry[key] = { status: 'unchanged', value: change.value };
-        break;
-      default:
-        break;
-    }
+const json = (diff) => JSON.stringify(
+  Object.entries(diff).reduce((acc, [key, change]) => {
+    const newEntry = {
+      [key]: (() => {
+        switch (change.type) {
+          case 'added':
+            return { status: 'added', value: change.value };
+          case 'removed':
+            return { status: 'removed', value: change.value };
+          case 'changed':
+            return {
+              status: 'changed',
+              oldValue: change.value1,
+              newValue: change.value2,
+            };
+          case 'unchanged':
+            return { status: 'unchanged', value: change.value };
+          default:
+            return {};
+        }
+      })(),
+    };
 
     return { ...acc, ...newEntry };
-  }, {});
-
-  return JSON.stringify(result, null, 2);
-};
+  }, {}),
+  null,
+  2,
+);
 
 export default json;
