d57a09547aabe6803436f9c9839d0a589372a87b
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-19 17:23:40 +0300

    fix muttation in sortiff

--- src/sortDiff.js
+++ src/sortDiff.js
@@ -1,11 +1,11 @@
 const sortObj = (obj) => {
-  const sortedKeys = Object.keys(obj).slice().sort();
-  return sortedKeys.reduce((acc, key) => {
-    acc[key] = typeof obj[key] === 'object' && obj[key] !== null
+  const sortedKeys = [...Object.keys(obj)].sort();
+  return sortedKeys.reduce((acc, key) => ({
+    ...acc,
+    [key]: typeof obj[key] === 'object' && obj[key] !== null
       ? sortObj(obj[key])
-      : obj[key];
-    return acc;
-  }, {});
+      : obj[key],
+  }), {});
 };
 
 export default sortObj;
