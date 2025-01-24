8a69a2eacd7ef624941ffee9b38a21582f2a81be
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-24 15:53:05 +0300

    delete sort logic and additional wrappers

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,19 +1,16 @@
 import findDifferences from '../diff.js';
-import sortObj from '../sortDiff.js';
 import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const compare = (obj1, obj2, format = stylish) => {
-  const sortObj1 = sortObj(obj1);
-  const sortObj2 = sortObj(obj2);
-  const diff = findDifferences(sortObj1, sortObj2);
+const compare = (obj1, obj2, format = 'stylish') => {
+  const diff = findDifferences(obj1, obj2);
 
-  if (format === stylish) {
-    return `{\n${stylish(diff)}\n}`;
-  } if (format === plain) {
-    return `${plain(diff).join('\n')}`;
-  } if (format === json) {
+  if (format === 'stylish') {
+    return stylish(diff);
+  } if (format === 'plain') {
+    return plain(diff);
+  } if (format === 'json') {
     return json(diff);
   }
   return `Unknown format: ${format}`;
