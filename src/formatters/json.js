e39d95848c27e770b290908a06a57df3fa31f300
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-24 15:53:55 +0300

    fix, because it has been changed diff.js

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,30 +1,35 @@
-const json = (diff) => JSON.stringify(
-  Object.entries(diff).reduce((acc, [key, change]) => {
-    const newEntry = {
-      [key]: (() => {
-        switch (change.type) {
-          case 'added':
-            return { status: 'added', value: change.value };
-          case 'removed':
-            return { status: 'removed', value: change.value };
-          case 'changed':
-            return {
-              status: 'changed',
-              oldValue: change.value1,
-              newValue: change.value2,
-            };
-          case 'unchanged':
-            return { status: 'unchanged', value: change.value };
-          default:
-            return {};
-        }
-      })(),
-    };
+const json = (diff) => {
+  const buildJson = (change) => {
+    switch (change.type) {
+      case 'added':
+        return { status: 'added', value: change.value };
+      case 'removed':
+        return { status: 'removed', value: change.value };
+      case 'changed':
+        return {
+          status: 'changed',
+          oldValue: change.value1,
+          newValue: change.value2,
+        };
+      case 'unchanged':
+        return { status: 'unchanged', value: change.value };
+      case 'nested': {
+        const children = Object.entries(change.children).reduce((acc, [key, childChange]) => {
+          acc[key] = buildJson(childChange);
+          return acc;
+        }, {});
+        return { children };
+      }
+      default:
+        return {};
+    }
+  };
+  const result = Object.entries(diff).reduce((acc, [key, change]) => {
+    acc[key] = buildJson(change);
+    return acc;
+  }, {});
 
-    return { ...acc, ...newEntry };
-  }, {}),
-  null,
-  2,
-);
+  return JSON.stringify(result, null, 2);
+};
 
 export default json;
