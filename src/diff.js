d8ae60b172bdb6fd849ece86dc69aa0645c65c69
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-24 15:55:28 +0300

    output three diffrent

--- src/diff.js
+++ src/diff.js
@@ -1,59 +1,32 @@
 import _ from 'lodash';
-import sortObj from './sortDiff.js';
 
-const findDifferences = (obj1, obj2, path = '') => {
+const findDifferences = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2));
   const sortedKeys = _.sortBy(keys);
+
   const differences = sortedKeys.reduce((acc, key) => {
-    const currentPath = path ? `${path}.${key}` : key;
-
     if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
-      return {
-        ...acc,
-        [currentPath]: { value: obj1[key], type: 'removed' },
-      };
-    }
-
-    if (obj1[key] !== obj2[key]) {
+      acc[key] = { type: 'removed', value: obj1[key] };
+    } else if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
+      acc[key] = { type: 'added', value: obj2[key] };
+    } else if (obj1[key] !== obj2[key]) {
       if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-        return {
-          ...acc,
-          [currentPath]: {
-            value1: obj1[key],
-            value2: obj2[key],
-            type: 'changed',
-          },
+        acc[key] = {
+          type: 'changed',
+          value1: obj1[key],
+          value2: obj2[key],
         };
+      } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        acc[key] = { type: 'nested', children: findDifferences(obj1[key], obj2[key]) };
+      } else {
+        acc[key] = { type: 'changed', value1: obj1[key], value2: obj2[key] };
       }
-
-      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        const nestedDifferences = findDifferences(obj1[key], obj2[key], currentPath);
-        return { ...acc, ...nestedDifferences };
-      }
-
-      return {
-        ...acc,
-        [currentPath]: { value1: obj1[key], value2: obj2[key], type: 'changed' },
-      };
-    }
-
-    return {
-      ...acc,
-      [currentPath]: { value: obj1[key], type: 'unchanged' },
-    };
-  }, {});
-
-  const addedKeys = Object.keys(obj2).reduce((acc, key) => {
-    const currentPath = path ? `${path}.${key}` : key;
-    if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
-      return {
-        ...acc,
-        [currentPath]: { value: obj2[key], type: 'added' },
-      };
+    } else {
+      acc[key] = { type: 'unchanged', value: obj1[key] };
     }
     return acc;
   }, {});
-  return sortObj({ ...differences, ...addedKeys });
+  return differences;
 };
 
 export default findDifferences;
