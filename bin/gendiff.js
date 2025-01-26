5946098219183bf064bd0b71cd06a359790e8334
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-26 14:01:00 +0300

    sring formatters => just function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import stylish from '../src/formatters/stylish.js';
 import choise from '../src/index.js';
 
 const program = new Command();
@@ -8,7 +9,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', stylish)
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,15 +3,15 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const compare = (obj1, obj2, format = 'stylish') => {
+const compare = (obj1, obj2, format = stylish) => {
   const diff = findDifferences(obj1, obj2);
 
   switch (format) {
-    case 'stylish':
+    case stylish:
       return stylish(diff);
-    case 'plain':
+    case plain:
       return plain(diff);
-    case 'json':
+    case json:
       return json(diff);
     default:
       return `Unknow format ${format}`;
