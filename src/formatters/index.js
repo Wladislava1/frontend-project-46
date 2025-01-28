560bc1f08ebb5ee2796708f81f33925458a54afb
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-01-28 12:48:05 +0300

    delete findDifferences

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,8 @@
-import findDifferences from '../diff.js';
 import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const compare = (obj1, obj2, format = 'stylish') => {
-  const diff = findDifferences(obj1, obj2);
-
+const compare = (diff, format = 'stylish') => {
   switch (format) {
     case 'stylish':
       return stylish(diff);
